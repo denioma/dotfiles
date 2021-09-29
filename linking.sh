4690951697769a96ccd1e00996ef4984921be2bb
Rodrigo Machado <29443508+denioma@users.noreply.github.com>
2021-09-29 11:31:32 +0100

    Added force flag

--- linking.sh
+++ linking.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # .dotfiles
-ln -s ~/.dotfiles/.bashrc ~/.bashrc
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -fs ~/.dotfiles/.bashrc ~/.bashrc
+ln -fs -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -fs ~/.dotfiles/.zshrc ~/.zshrc
 if [[ ! "$(uname -r)" =~ ".*WSL2$" ]]; then
-    ln -s ~/.dotfiles/wsl2/.gitconfig ~/.gitconfig;
+    ln -fs ~/.dotfiles/wsl2/.gitconfig ~/.gitconfig;
 else
-    ln -s ~/.dotfiles/.gitconfig ~/.gitconfig;
+    ln -fs ~/.dotfiles/.gitconfig ~/.gitconfig;
 fi
 
 # .dotfiles/oh-my-zsh
-ln -s ~/.dotfiles/.oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
+ln -fs ~/.dotfiles/.oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
