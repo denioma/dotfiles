7b83bb36e7ed609e450de918cd031ea73f04a5d2
Rodrigo Machado <29443508+denioma@users.noreply.github.com>
2021-09-29 11:07:11 +0100

    WSL2 specific config

--- linking.sh
+++ linking.sh
@@ -2,9 +2,13 @@
 
 # .dotfiles
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+if [[ ! "$(uname -r)" =~ ".*WSL2$" ]]; then
+    ln -s ~/.dotfiles/wsl2/.gitconfig ~/.gitconfig;
+else
+    ln -s ~/.dotfiles/.gitconfig ~/.gitconfig;
+fi
 
 # .dotfiles/oh-my-zsh
 ln -s ~/.dotfiles/.oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
