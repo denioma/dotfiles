41cab1d1781a3b2d2f95f92c5fa27447a4c54273
Rodrigo Machado <29443508+denioma@users.noreply.github.com>
2021-09-29 10:29:20 +0100

    First commit

---
+++ linking.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# .dotfiles
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+
+# .dotfiles/oh-my-zsh
+ln -s ~/.dotfiles/.oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
